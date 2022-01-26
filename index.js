db7c0b9cfd299204e44713aa19e70a4489b2f860
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-01-26 15:47:45 -0300

    Set mock routes of API

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+app.post("/participants", (req, res) => {
+  res.send("Mock route to POST /participants");
+});
+
+app.get("/participants", (req, res) => {
+  res.send("Mock route to GET /participants");
+});
+
+app.post("/messages", (req, res) => {
+  res.send("Mock route to POST /messages");
+});
+
+app.get("/messages", (req, res) => {
+  res.send("Mock route to GET /messages");
+});
+
+app.post("/status", (req, res) => {
+  res.send("Mock route to POST /status");
+});
+
 app.listen(4000, () => {
   console.log("Rodando em http://localhost:4000");
 });
