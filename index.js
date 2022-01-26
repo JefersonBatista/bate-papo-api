66786975c1cbc690ecc0532a4dca80d1e864b235
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-01-26 15:12:14 -0300

    Configure basic DB client

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express, { json } from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbClient = new MongoClient(process.env.MONGO_URI);
 
 const app = express();
 app.use(cors());
